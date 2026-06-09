14803dbf74b392ad7b5ac895ed0f8ab31cdd2c46
imran <uffmink@gmail.com>
2026-06-09 20:50:32 +0300

    refactor database connection logic for local and production environments

--- config/db.php
+++ config/db.php
@@ -1,14 +1,18 @@
 <?php
 
-$conn = new mysqli(
-    "sql303.infinityfree.com",
-    "if0_42136640",
-    "Fdj1PvxxJh",
-    "if0_42136640_XXX"
-);
+$isLocal = $_SERVER['SERVER_NAME'] === 'localhost';
+
+if ($isLocal) {
+    $conn = new mysqli("localhost", "root", "", "blog");
+} else {
+    $conn = new mysqli(
+        "sql303.infinityfree.com",
+        "if0_42136640",
+        "Fdj1PvxxJh",
+        "if0_42136640_blog"
+    );
+}
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-$conn->set_charset("utf8mb4");
