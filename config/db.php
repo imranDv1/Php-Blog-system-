f75fc9ba37148bdcf5bf4c2ad47cfa57f5a98447
imran <uffmink@gmail.com>
2026-06-09 10:54:17 +0300

    fix

--- config/db.php
+++ config/db.php
@@ -1,6 +1,14 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "blog");
+
+$conn = new mysqli(
+    "sql303.infinityfree.com",
+    "if0_42136640",
+    "Fdj1PvxxJh",
+    "if0_42136640_XXX"
+);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+$conn->set_charset("utf8mb4");
